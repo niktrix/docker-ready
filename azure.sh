f2f94ea56e7d39a1cb4757880caebf5e8b161788
nicky g <nicky.gurbani@gmail.com>
2016-02-28 10:36:12 +0530

    docker compose
    install docker compose

--- azure.sh
+++ azure.sh
@@ -1,2 +1,11 @@
 apt-get update
 curl -fsSL https://get.docker.com/ | sh
+
+#docker compose
+sudo -i
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
+
+
